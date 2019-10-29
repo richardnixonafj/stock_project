67b24d14c391f81bc42d0593eec3493bbd1897fc
Richard Nixon <richardnixon@posteo.org>
2019-10-28 22:10:57 -0300

    Criar app core. Close #4

--- projeto/settings.py
+++ projeto/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOST', default=[], cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
 
 # Application definition
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # meus_apps
+    'projeto.core',
 ]
 
 MIDDLEWARE = [
